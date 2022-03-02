f31be38752a4d38497f2d4e58e1ddec9f0c5a083
Jorge Adalberto Rodriguez Garcia <george_nlc@hotmail.com>
2022-03-01 22:10:39 -0600

    Se añadio un archivo llamado beers.js y se desarrollo la app

---
+++ beers.js
@@ -0,0 +1,11 @@
+let message = 'beers'
+let beers = 99
+
+while (beers > 0) {
+    console.log('There are ' + message + ' in the wall')
+    console.log('Pass one around')
+    beers --
+    console.log(beers + message + ' left in the wall')
+}
+
+console.log(beers + message + 'left in the wall.')
