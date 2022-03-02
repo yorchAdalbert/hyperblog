af8ce42c72a82107e169286ccaeb835a5dc0cc6d
Jorge Adalberto Rodriguez Garcia <george_nlc@hotmail.com>
2022-03-01 22:14:39 -0600

    Se modifico el output del app

--- beers.js
+++ beers.js
@@ -2,10 +2,8 @@ let message = 'beers'
 let beers = 99
 
 while (beers > 0) {
-    console.log('There are ' + message + ' in the wall')
+    console.log('There are ' + beers + ' ' + message + ' in the wall')
     console.log('Pass one around')
     beers --
-    console.log(beers + message + ' left in the wall')
+    console.log(beers + ' ' + message + ' left in the wall')
 }
-
-console.log(beers + message + 'left in the wall.')
